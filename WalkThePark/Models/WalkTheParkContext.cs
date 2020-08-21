2d1a968758e6b73a94d78cc4f70be84971fed132
Taylor Phillips <taylorphillips133@gmail.com>
2020-08-21 13:25:00 -0700

    continue to build out some seeded entries for the API

--- WalkThePark/Models/WalkTheParkContext.cs
+++ WalkThePark/Models/WalkTheParkContext.cs
@@ -14,17 +14,17 @@ namespace WalkThePark.Models
     {
       builder.Entity<NationalPark>()
       .HasData(
-        new NationalPark { NationalParkId = 1, Name = "Crater_Lake", State = "Oregon", AccessPrice = 30},
-        new NationalPark { NationalParkId = 2, Name = "Oregon_Caves", State = "Oregon", AccessPrice = 10},
-        new NationalPark { NationalParkId = 3, Name = "Fort_Vancouver", State = "Washington", AccessPrice = 10},
-        new NationalPark { NationalParkId = 4, Name = "San_Juan", State = "Washington", AccessPrice = 0},
-        new NationalPark { NationalParkId = 5, Name = "Point_Reyes", State = "California", AccessPrice = 0},
-        new NationalPark { NationalParkId = 6, Name = "Golden_Gate", State = "California", AccessPrice = 0}
+        new NationalPark { NationalParkId = 1, Name = "Crater_Lake_National_Park", State = "Oregon", AccessPrice = 30},
+        new NationalPark { NationalParkId = 2, Name = "Oregon_Caves_National_Park", State = "Oregon", AccessPrice = 10},
+        new NationalPark { NationalParkId = 3, Name = "Fort_Vancouver_National_Park", State = "Washington", AccessPrice = 10},
+        new NationalPark { NationalParkId = 4, Name = "San_Juan_National_Park", State = "Washington", AccessPrice = 0},
+        new NationalPark { NationalParkId = 5, Name = "Point_Reyes_National_Park", State = "California", AccessPrice = 0},
+        new NationalPark { NationalParkId = 6, Name = "Golden_Gate_National_Park", State = "California", AccessPrice = 0}
       );
       builder.Entity<StatePark>()
       .HasData(
-        new StatePark { StateParkId = 1, Name = "", State = "Oregon", County = ""},
-        new StatePark { StateParkId = 2, Name = "", State = "Oregon", County = "" },
+        new StatePark { StateParkId = 1, Name = "Bandon_State_Natural_Area", State = "Oregon", County = "Coos"},
+        new StatePark { StateParkId = 2, Name = "Cascadia_State_Park", State = "Oregon", County = "" },
         new StatePark { StateParkId = 3, Name = "", State = "Washington", County = "" },
         new StatePark { StateParkId = 4, Name = "", State = "Washington", County = "" },
         new StatePark { StateParkId = 5, Name = "", State = "California", County = "" },
