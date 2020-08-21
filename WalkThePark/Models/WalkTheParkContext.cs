cfbc4f4b128d76716fcfea4211700bf38eeab162
Taylor Phillips <taylorphillips133@gmail.com>
2020-08-21 15:10:30 -0700

    add more seedings to the context file since I've confirmed search and paination are routing correctly

--- WalkThePark/Models/WalkTheParkContext.cs
+++ WalkThePark/Models/WalkTheParkContext.cs
@@ -19,7 +19,11 @@ namespace WalkThePark.Models
         new NationalPark { NationalParkId = 3, Name = "Fort_Vancouver_National_Park", State = "Washington", AccessPrice = 10},
         new NationalPark { NationalParkId = 4, Name = "San_Juan_National_Park", State = "Washington", AccessPrice = 0},
         new NationalPark { NationalParkId = 5, Name = "Point_Reyes_National_Park", State = "California", AccessPrice = 0},
-        new NationalPark { NationalParkId = 6, Name = "Golden_Gate_National_Park", State = "California", AccessPrice = 0}
+        new NationalPark { NationalParkId = 6, Name = "Golden_Gate_National_Park", State = "California", AccessPrice = 0},
+        new NationalPark { NationalParkId = 7, Name = "John_Day_Fossil_Beds", State = "Oregon", AccessPrice = 0 },
+        new NationalPark { NationalParkId = 8, Name = "Oregon_Dunes_National_Recreation_Area", State = "Oregon", AccessPrice = 0},
+        new NationalPark { NationalParkId = 9, Name = "Mount_Rainier_National_Park", State = "Washington", AccessPrice = 15 },
+        new NationalPark { NationalParkId = 10, Name = "Yosemite_National_Park", State = "California", AccessPrice = 15 }
       );
       builder.Entity<StatePark>()
       .HasData(
@@ -28,7 +32,11 @@ namespace WalkThePark.Models
         new StatePark { StateParkId = 3, Name = "Fort_Ebey_State_Park", State = "Washington", County = "Island" },
         new StatePark { StateParkId = 4, Name = "Ginkgo_Petrified_Forest", State = "Washington", County = "Kittitas" },
         new StatePark { StateParkId = 5, Name = "Emerald_Bay_State_Park", State = "California", County = "El_Dorado" },
-        new StatePark { StateParkId = 6, Name = "Grizzly_Creek_Redwoods", State = "California", County = "Humboldt" }
+        new StatePark { StateParkId = 6, Name = "Grizzly_Creek_Redwoods", State = "California", County = "Humboldt" },
+        new StatePark { StateParkId = 7, Name = "Champoeg_State_Heritage_Area", State = "Oregon", County = "Marion"},
+        new StatePark { StateParkId = 8, Name = "Milo_McIver_State_Park", State = "Oregon", County = "Clackamas" },
+        new StatePark { StateParkId = 9, Name = "Beacon_Rock_State_Park", State = "Washington", County = "Skamania" },
+        new StatePark { StateParkId = 10, Name = "Castle_Rock_State_Park", State = "California", County = "Santa_Cruz" }
       );
     }
     public DbSet<NationalPark> NationalParks { get; set; }
